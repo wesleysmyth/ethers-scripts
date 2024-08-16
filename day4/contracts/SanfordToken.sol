88dd8c53c0efcea2315910b81a14e5337845bbf8
Wesley Smith <wesley.smith@Kristines-MacBook-Pro.local>
2024-08-16 15:05:26 -0400

    Add additional methods / tests

--- day4/contracts/SanfordToken.sol
+++ day4/contracts/SanfordToken.sol
@@ -2,11 +2,31 @@
 pragma solidity ^0.8.24;
 
 contract SanfordToken {
+    address public immutable owner;
     uint256 public constant totalSupply = 1000;
     uint256 public totalCreated = 0;
+    uint256 public constant MINT_PRICE = 0.01 ether;
     mapping(address => uint256) public balances;
+    event Buy(address indexed buyer);
 
-    function create(uint256 quantity) public {
+    struct Vote {
+        address voter;
+        bool selection;
+    }
+
+    Vote[] public votes;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function create(uint256 quantity) public onlyOwner {
         require(totalCreated + quantity <= totalSupply, "Cannot create more tokens than the total supply");
 
         balances[ msg.sender ] += quantity;
@@ -18,4 +38,22 @@ contract SanfordToken {
         balances[ msg.sender ] -= quantity;
         balances[ to ] += quantity;
     }
+
+    function buy() public payable {
+        require(totalCreated + 1 < totalSupply, "Cannot create more tokens than the total supply");
+        require(msg.value == MINT_PRICE, "Incorrect amount sent");
+
+        balances[ msg.sender ] += 1;
+        totalCreated += 1;
+        emit Buy(msg.sender);
+    }
+
+    function withdraw() onlyOwner public {
+        (bool sent, bytes memory data) = owner.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
--- day4/test/Token.js
+++ day4/test/Token.js
@@ -31,8 +31,40 @@ describe("SanfordToken", function () {
         expect(signer0Balance).to.equal(100);
     });
 
-    // it("Should return the initial total created as zero", async function () {
-        
-    //     expect(count).to.equal(0);
-    // });
+    it("Should revert if creating more than the total supply the balance of the creator as expected", async function () {
+        await expect(sanfordToken.create(1000)).to.be.revertedWith('Cannot create more tokens than the total supply');
+    });
+
+    it("Should send the correct amount of tokens and update balances", async function () {
+        const sendTx = await sanfordToken.send(signer1.address, 25);
+        sendTx.wait();
+        const signer0Balance = await sanfordToken.balances(signer0.address);
+        const signer1Balance = await sanfordToken.balances(signer1.address);
+        expect(signer0Balance).to.equal(75);
+        expect(signer1Balance).to.equal(25);
+    });
+
+    it("Should allow someone to buy a token for the mint price", async function () {
+        const MINT_PRICE = await sanfordToken.MINT_PRICE();
+        const buyTx = await sanfordToken.buy({
+            value: MINT_PRICE
+        });
+        await buyTx.wait();
+        const balance = await sanfordToken.balances(signer0.address);
+        expect(balance).to.equal(76);
+    });
+
+    it("Should not allow someone to buy a token for above the mint price", async function () {
+        const MINT_PRICE = await sanfordToken.MINT_PRICE();
+        await expect(sanfordToken.buy({
+            value: MINT_PRICE + ethers.parseEther('0.001')
+        })).to.be.revertedWith('Incorrect amount sent');
+    });
+
+    it("Should not allow someone to buy a token for below the mint price", async function () {
+        const MINT_PRICE = await sanfordToken.MINT_PRICE();
+        await expect(sanfordToken.buy({
+            value: MINT_PRICE - ethers.parseEther('0.001')
+        })).to.be.revertedWith('Incorrect amount sent');
+    });
 });
